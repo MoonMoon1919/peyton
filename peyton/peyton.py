d89d658ed4e9c2d1f3ddd55467eada69fe294717
Max Moon <moon.maxwell@gmail.com>
2019-12-01 11:44:36 -0800

    update readme and add examples.

--- peyton/peyton.py
+++ peyton/peyton.py
@@ -1,1 +1,30 @@
 """Peyton main entrypoint."""
+
+from router import Router
+
+
+# Singleton
+router = Router()
+
+"""
+Example:
+
+from peyton.view import ViewBase
+
+@router.register(path="/")
+class Index(ViewBase):
+    def get(self, data):
+        print("get from index")
+
+    def put(self, data):
+        print("put from index")
+
+    def post(self, data):
+        print("post from index")
+
+
+# a very basic stripped down event
+event = {"httpMethod": "DELETE", "path": "/"}
+
+print(router.dispatch(event))
+"""

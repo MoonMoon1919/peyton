7304897ceaeb934a93fdf4cc04ef16905b5e375b
Max Moon <moon.maxwell@gmail.com>
2019-12-26 20:32:39 -0800

    add scaffolding for view tests

---
+++ tests/test_view.py
@@ -0,0 +1,14 @@
+"""."""
+
+import sys
+from os import path
+import json
+
+from peyton.view import ViewBase
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+def test_view():
+    """."""
+    pass

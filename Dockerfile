6795d3908867a006a47d918782f86d0674cb038a
Max Moon <moon.maxwell@gmail.com>
2019-12-27 16:47:21 -0800

    add setup.py

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.8.1-alpine3.11
 
-RUN apk add --update build-base bash && pip install pipenv
+RUN apk add --update build-base bash libffi-dev openssl-dev && pip install pipenv setuptools twine
 
 RUN mkdir app
 
---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup module for building Peyton."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="peyton",
+    version="0.1.0",
+    packages=find_packages(exclude=["test*"]),
+    license="MIT",
+    long_description=open("README.md").read(),
+    author="M. Moon",
+    description="A lightweight framework for AWS Lambda for building Rest APIs",
+    author_email="moon.maxwell@gmail.com",
+)

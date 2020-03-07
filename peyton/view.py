1f528c26557407d7a0e29f3a3a737b6b944da2c9
Max Moon <moon.maxwell@gmail.com>
2020-03-07 09:29:08 -0800

    cleanup unnecessary init method in view.py

--- peyton/view.py
+++ peyton/view.py
@@ -22,9 +22,6 @@ class ViewBase:
               if a supported HTTP method verb does not exist
     """
 
-    def __init__(self):
-        current_request = None
-
     def get(self, **kwargs) -> ResponseObject:
         resp = Response(
             status_code=405, headers={}, body={"message": "Method not implemented"}
